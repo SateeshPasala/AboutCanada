f7910182dda44ac37158843a562d84b713d4e173
Veera Venkata Sateesh Pasala <veeravenkatasateesh.pasala@blackboard.com>
2020-10-31 10:14:10 +0530

    Added Model Classes

---
+++ AboutCanada/Model/Asset.swift
@@ -0,0 +1,15 @@
+//
+//  Asset.swift
+//  AboutCanada
+//
+//  Created by Veera Venkata Sateesh Pasala on 31/10/20.
+//  Copyright © 2020 Veera Venkata Sateesh Pasala. All rights reserved.
+//
+
+import Foundation
+struct Asset: Codable {
+ 
+    let title: String?
+    let description: String?
+    let imageHref: String?
+}
---
+++ AboutCanada/Model/CellData.swift
@@ -0,0 +1,16 @@
+//
+//  CellData.swift
+//  AboutCanada
+//
+//  Created by Veera Venkata Sateesh Pasala on 31/10/20.
+//  Copyright © 2020 Veera Venkata Sateesh Pasala. All rights reserved.
+//
+
+import Foundation
+
+struct CellData: Codable {
+    let title : String?
+    let rows : [Asset]?
+
+}
+
