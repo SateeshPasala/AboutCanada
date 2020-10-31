95fa6bdf5c01524f64ea224e57030f06f626e48d
Veera Venkata Sateesh Pasala <veeravenkatasateesh.pasala@blackboard.com>
2020-10-31 10:35:07 +0530

    Created Custom cell , and Populated sample data before creating network classes .

---
+++ AboutCanada/InfoTableViewCell.swift
@@ -0,0 +1,89 @@
+//
+//  InfoTableViewCell.swift
+//  AboutCanada
+//
+//  Created by Veera Venkata Sateesh Pasala on 31/10/20.
+//  Copyright © 2020 Veera Venkata Sateesh Pasala. All rights reserved.
+//
+
+import UIKit
+
+
+
+class InfoTableViewCell: UITableViewCell {
+    
+
+       var cellImage = UIImageView()
+        let title = UILabel()
+        let descreption = UILabel()
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.init(white: 1.0, alpha: 0.1)
+        selectedBackgroundView = backgroundView
+       
+        contentView.addSubview(cellImage)
+        contentView.addSubview(title)
+        contentView.addSubview(descreption)
+ 
+        
+        configLabels()
+        installConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    private func installConstraints() {
+        
+        
+        contentView.heightAnchor.constraint(greaterThanOrEqualToConstant:80).isActive = true
+        
+        cellImage.translatesAutoresizingMaskIntoConstraints = false
+        cellImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8).isActive = true
+        cellImage.widthAnchor.constraint(equalToConstant: 62).isActive = true
+        cellImage.heightAnchor.constraint(equalToConstant: 62).isActive = true
+        cellImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8).isActive = true
+
+        
+        
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8).isActive = true
+        title.leadingAnchor.constraint(equalTo: cellImage.trailingAnchor, constant: 8).isActive = true
+        title.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8).isActive = true
+        
+        descreption.translatesAutoresizingMaskIntoConstraints = false
+        descreption.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 8).isActive = true
+        descreption.leadingAnchor.constraint(equalTo: title.leadingAnchor, constant: 0).isActive = true
+        descreption.trailingAnchor.constraint(equalTo: title.trailingAnchor, constant: 0).isActive = true
+        descreption.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8).isActive = true
+        
+    }
+    
+    
+    
+    private func configLabels() {
+        title.isUserInteractionEnabled = false
+        descreption.numberOfLines = 0
+        title.numberOfLines = 1
+        cellImage.contentMode =  .scaleAspectFit;
+        
+    }
+
+}
--- AboutCanada/MainViewController.swift
+++ AboutCanada/MainViewController.swift
@@ -7,24 +7,106 @@
 //
 
 import UIKit
+private let CellID = #file
 
 class MainViewController: UIViewController {
+    
+    var tableview  =  UITableView()
+    var safeArea: UILayoutGuide!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        setUpView()
+
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        safeArea = view.layoutMarginsGuide
+        setNavigationBar()
+        setupTableView()
+        
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableview)
+        tableview.translatesAutoresizingMaskIntoConstraints = false
+        tableview.topAnchor.constraint(equalTo: view.topAnchor, constant: 70).isActive = true
+        tableview.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableview.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
+    func setNavigationBar() {
+        let screenSize: CGRect = UIScreen.main.bounds
+        let navBar = UINavigationBar(frame: CGRect(x: 0, y:20, width: screenSize.width, height: 44))
+        let navItem = UINavigationItem(title: "Canada")
+        navBar.setItems([navItem], animated: false)
+        self.view.addSubview(navBar)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        setNavigationBar()
     }
-    */
+    
+    
+    
+    
+    private func setUpView() {
+        
+        /// Setting up navigation bar
+        title = "Canada"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
 
+        
+        tableview.delegate = self
+        tableview.register(InfoTableViewCell.self, forCellReuseIdentifier: CellID)
+        tableview.dataSource = self
+        tableview.rowHeight = UITableView.automaticDimension
+        tableview.estimatedRowHeight = 100
+        tableview.allowsSelection = false
+        tableview.accessibilityIdentifier = "tableView"
+    }
+    
+    
+
+    
+    
 }
+
+extension MainViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+    
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CellID, for: indexPath) as? InfoTableViewCell
+            else { fatalError() }
+        cell.cellImage.image = nil
+        cell.title.text = "Sample text "
+        cell.descreption.text = "Sample description"
+        return cell
+    }
+}
+
+
+extension MainViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
