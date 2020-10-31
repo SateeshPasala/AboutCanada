f10d679c746d1ab833b98fb42165a3fda438e2be
Veera Venkata Sateesh Pasala <veeravenkatasateesh.pasala@blackboard.com>
2020-10-31 09:58:30 +0530

    Initial Commit   Deleted the default view Controller and story board and added MainVC

---
+++ AboutCanada/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  AboutCanada
+//
+//  Created by Veera Venkata Sateesh Pasala on 31/10/20.
+//  Copyright © 2020 Veera Venkata Sateesh Pasala. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
